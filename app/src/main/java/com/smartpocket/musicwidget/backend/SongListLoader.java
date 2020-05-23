5d9db4139fe3df82a74cb34795077952f4017224
Esteban Lagord <estebanlagord@gmail.com>
2020-05-22 21:55:20 -0300

    Removed WhatsApp files from result

--- app/src/main/java/com/smartpocket/musicwidget/backend/SongListLoader.java
+++ app/src/main/java/com/smartpocket/musicwidget/backend/SongListLoader.java
@@ -6,20 +6,20 @@ import android.provider.MediaStore;
 import android.util.Log;
 
 public class SongListLoader {
-	private static final String TAG = "Music Loader";
-	private static final String SORT_BY = MediaStore.Audio.Media.ARTIST + ", " + MediaStore.Audio.Media.TITLE;
-	private static SongListLoader instance;
-	private final Context context;
-	private Cursor cur;
+    private static final String TAG = "Music Loader";
+    private static final String SORT_BY = MediaStore.Audio.Media.ARTIST + ", " + MediaStore.Audio.Media.TITLE;
+    private static SongListLoader instance;
+    private final Context context;
+    private Cursor cur;
 
 
-	public static SongListLoader getInstance(Context context){
-		if (instance == null) {
-			instance = new SongListLoader(context);
-		}
+    public static SongListLoader getInstance(Context context) {
+        if (instance == null) {
+            instance = new SongListLoader(context);
+        }
 
-		return instance;
-	}
+        return instance;
+    }
 
     private SongListLoader(Context context) {
         this.context = context;
@@ -30,22 +30,23 @@ public class SongListLoader {
         Log.d(TAG, "Querying media...");
 
 
-		//Some audio may be explicitly marked as not being music
-		String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+        //Some audio may be explicitly marked as not being music
+        String selection = getDefaultSelection();
 
-		String[] projection = {
-		        MediaStore.Audio.Media._ID,
-		        MediaStore.Audio.Media.ARTIST,
-		        MediaStore.Audio.Media.TITLE,
-		        MediaStore.Audio.Media.DURATION
-		};
+        String[] projection = {
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.DATA
+        };
 
-		cur = context.getContentResolver().query(
-		        MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-		        projection,
-		        selection,
-		        null,
-				SORT_BY);
+        cur = context.getContentResolver().query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                selection,
+                null,
+                SORT_BY);
 
         Log.d(TAG, "Query finished. " + (cur == null ? "Returned NULL." : "Returned a cursor."));
 
@@ -56,41 +57,46 @@ public class SongListLoader {
         }
 
         Log.d(TAG, "Done querying media. SongListLoader is ready.");
-		return cur;
+        return cur;
     }
 
-	public Cursor getFilteredCursor(CharSequence constraint) {
-		Log.d(TAG, "Querying media for filter...");
+    private String getDefaultSelection() {
+        return MediaStore.Audio.Media.IS_MUSIC + " != 0 and " +
+                MediaStore.Audio.Media.DATA + " NOT LIKE '%/WhatsApp/%'";
+    }
+
+    public Cursor getFilteredCursor(CharSequence constraint) {
+        Log.d(TAG, "Querying media for filter...");
 
 
-		//Some audio may be explicitly marked as not being music
-		String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0 and "
-				+ "( " + MediaStore.Audio.Media.ARTIST + " LIKE ? or "
-				+ MediaStore.Audio.Media.TITLE + " LIKE ? )";
+        //Some audio may be explicitly marked as not being music
+        String selection = getDefaultSelection() + " and "
+                + "( " + MediaStore.Audio.Media.ARTIST + " LIKE ? or "
+                + MediaStore.Audio.Media.TITLE + " LIKE ? )";
 
-		String[] projection = {
-				MediaStore.Audio.Media._ID,
-				MediaStore.Audio.Media.ARTIST,
-				MediaStore.Audio.Media.TITLE,
-				MediaStore.Audio.Media.DURATION
-		};
+        String[] projection = {
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DURATION
+        };
 
-		cur = context.getContentResolver().query(
-				MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-				projection,
-				selection,
-				new String[] { "%"+constraint+"%", "%"+constraint+"%" },
-				SORT_BY);
+        cur = context.getContentResolver().query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                selection,
+                new String[]{"%" + constraint + "%", "%" + constraint + "%"},
+                SORT_BY);
 
-		Log.d(TAG, "Query for filter finished. " + (cur == null ? "Returned NULL." : "Returned a cursor."));
+        Log.d(TAG, "Query for filter finished. " + (cur == null ? "Returned NULL." : "Returned a cursor."));
 
-		if (cur == null) {
-			// Query failed...
-			Log.e(TAG, "Failed to retrieve music: cursor is null");
-			return null;
-		}
+        if (cur == null) {
+            // Query failed...
+            Log.e(TAG, "Failed to retrieve music: cursor is null");
+            return null;
+        }
 
-		Log.d(TAG, "Done querying media. SongListLoader is ready.");
-		return cur;
-	}
+        Log.d(TAG, "Done querying media. SongListLoader is ready.");
+        return cur;
+    }
 }
